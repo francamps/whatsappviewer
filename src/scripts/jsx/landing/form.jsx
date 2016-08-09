c2550fc1d1673c4a7e8dc07fc5ae9beec93dba42
Franc <francamps@gmail.com>
2016-08-09 13:38:32 +0200

    'How do I do this' shows instructions on landing

--- src/scripts/jsx/landing/form.jsx
+++ src/scripts/jsx/landing/form.jsx
@@ -7,6 +7,9 @@ import Notification from '../notification';
 export default class Form extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      isShowingInstructions: false
+    }
   }
 
   renderNotification () {
@@ -18,11 +21,21 @@ export default class Form extends React.Component {
     }
   }
 
+  showInstructions () {
+    this.setState({
+      isShowingInstructions: !this.state.isShowingInstructions
+    });
+  }
+
   render () {
     return (
       <div id="form-container" className="form-container">
         <div className="widget">
-          <p>Email yourself your conversation from your app and paste the text of it here. (<span>How do I do this?</span>)</p>
+          <p>
+            Email yourself your conversation from your app and paste the text of it here.
+            (<span className="show-instructions" onClick={this.showInstructions.bind(this)}>How do I do this?</span>)
+          </p>
+          <Instructions isShowing={this.state.isShowingInstructions}/>
           <ChatForm onClickRender={this.props.onClickRender}/>
           {this.renderNotification()}
         </div>
--- src/scripts/jsx/landing/instructions.jsx
+++ src/scripts/jsx/landing/instructions.jsx
@@ -1,12 +1,16 @@
 'use strict';
 
 export default class Instructions extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
   render () {
+    let classes = "instructions";
+    classes += (this.props.isShowing) ? " showing" : " hidden";
+
     return (
-      <div className="instructions widget">
-        <h2>Get some insight into your daily WhatsApp conervsations</h2>
-        <p>Email yourself your conversation from your app and paste the text of it here. (<span>How do I do this?</span>)</p>
-        <h3>How to use this</h3>
+      <div className={classes}>
         <ol>
           <li>Open your Whatsapp and enter the conversation you'd like to explore.</li>
           <li>On the top right, tap on the settings icon (a three-dot icon)
